e93fb4024c5ff4f732e2785e4d3b6eca9d6570d5
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-10-15 11:26:06 +0100

    Basic cube generation using 3 nested loop

---
+++ Assets/scripts/WorldController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour {
+
+    public GameObject block;
+    public int worldSize = 2;
+
+    private void Start()
+    {
+        StartCoroutine(BuildWorld());
+    }
+
+    public IEnumerator BuildWorld()
+    {
+        for(int z = 0; z < worldSize; z++)
+        {
+            for(int y = 0; y < worldSize; y++)
+            {
+                for(int x = 0; x < worldSize; x++)
+                {
+                    Vector3 pos = new Vector3(x, y, z);
+                    GameObject cube = Instantiate(block, pos, Quaternion.identity);
+                    cube.name = (x + "_" + y + "_" + z);
+                    cube.transform.parent = gameObject.transform;
+                }
+                yield return null;
+            }
+        }
+    }
+
+}
