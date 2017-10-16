a454cd49e9bf10ed93eb678d9032f965751195bf
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-10-17 00:30:43 +0100

    Create cube

--- Assets/scripts/CreateQuads.cs
+++ Assets/scripts/CreateQuads.cs
@@ -2,14 +2,33 @@
 
 public class CreateQuads : MonoBehaviour {
 
+    enum CubeSide {
+        BOTTOM,
+        TOP,
+        LEFT,
+        RIGHT,
+        FRONT,
+        BACK
+    };
+
     public Material cubeMat;
 
     private void Start()
     {
-        CreateQuad();
+        CreateCube();
     }
 
-    private void CreateQuad()
+    private void CreateCube()
+    {
+        CreateQuad(CubeSide.BOTTOM);
+        CreateQuad(CubeSide.TOP);
+        CreateQuad(CubeSide.LEFT);
+        CreateQuad(CubeSide.RIGHT);
+        CreateQuad(CubeSide.FRONT);
+        CreateQuad(CubeSide.BACK);
+    }
+
+    private void CreateQuad(CubeSide cubeSide)
     {
         Mesh mesh = new Mesh();
         mesh.name = "ScriptedMesh";
@@ -35,25 +54,45 @@ public class CreateQuads : MonoBehaviour {
         Vector2 uv01 = new Vector2(0f, 1f);
         Vector2 uv11 = new Vector2(1f, 1f);
 
-        // These are the vertices of the front facing face
-        vertices = new Vector3[] {
-            p4,
-            p5,
-            p1,
-            p0
-        };
-
-        uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
-
-        // All normals are facing forward from the given face
-        normals = new Vector3[] {
-            Vector3.forward,
-            Vector3.forward,
-            Vector3.forward,
-            Vector3.forward
-        };
-
-        triangles = new int[] { 3, 1, 0, 3, 2, 1 };
+        switch(cubeSide)
+        {
+            case CubeSide.BOTTOM:
+                vertices = new Vector3[] { p0, p1, p2, p3 };
+                normals = new Vector3[] { Vector3.down, Vector3.down, Vector3.down, Vector3.down };
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1};
+                break;
+            case CubeSide.TOP:
+                vertices = new Vector3[] { p7, p6, p5, p4 };
+                normals = new Vector3[] { Vector3.up, Vector3.up, Vector3.up, Vector3.up };
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1};
+                break;
+            case CubeSide.LEFT:
+                vertices = new Vector3[] { p7, p4, p0, p3 };
+                normals = new Vector3[] { Vector3.left, Vector3.left, Vector3.left, Vector3.left };
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1};
+                break;
+            case CubeSide.RIGHT:
+                vertices = new Vector3[] { p5, p6, p2, p1 };
+                normals = new Vector3[] { Vector3.right, Vector3.right, Vector3.right, Vector3.right };
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1};
+                break;
+            case CubeSide.FRONT:
+                vertices = new Vector3[] { p4, p5, p1, p0 };
+                normals = new Vector3[] { Vector3.forward, Vector3.forward, Vector3.forward, Vector3.forward };
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1};
+                break;
+            case CubeSide.BACK:
+                vertices = new Vector3[] { p6, p7, p3, p2 };
+                normals = new Vector3[] { Vector3.back, Vector3.back, Vector3.back, Vector3.back };
+                uvs = new Vector2[] { uv11, uv01, uv00, uv10 };
+                triangles = new int[] { 3, 1, 0, 3, 2, 1};
+                break;
+        }
 
         mesh.vertices = vertices;
         mesh.uv = uvs;
